f1a6f29da661f2852298aa58b5dad88f811a651b
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2025-06-10 21:33:49 -0400

    reading request and headers, not body

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
 	"net"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+)
+
+var (
+	activeConns = make(map[net.Conn]struct{}) // to track connections
+	mu          sync.Mutex                    // to protect map
 )
 
 func main() {
 	// Listen for incoming connection
 	listener, err := net.Listen("tcp", "localhost:8080")
-	// Telling the os that I only want handle tcp traffic. and listen on port 8080
+	// Telling the os that I only want handle tcp traffic. and listen on port 8080, this socket is passive, listener socket
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
@@ -17,13 +28,46 @@ func main() {
 
 	fmt.Println("Server is listening on port 8080")
 
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+
+		<-ctx.Done()
+
+		fmt.Println("\nShutting down gracefully...")
+
+		// Stop accepting new connections
+		listener.Close()
+
+		mu.Lock()
+		for conn := range activeConns {
+			conn.Close()
+		}
+		mu.Unlock()
+	}()
+
 	for {
 		// Accept incoming connections
+		// Creates a new socket, different from listening socket from above
+		// OS creates a new connection socket for that client.
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error:", err)
-			continue
+			select {
+			case <-ctx.Done():
+				// Context was cancel, exit
+				fmt.Println("Server shutdown complete")
+				return
+
+			default:
+				fmt.Println("Accept error:", err)
+				continue
+			}
 		}
+
+		mu.Lock()
+		activeConns[conn] = struct{}{}
+		mu.Unlock()
 
 		// Handle client connection in goroutine
 		go handleClient(conn)
@@ -31,29 +75,51 @@ func main() {
 }
 
 func handleClient(conn net.Conn) {
-	defer conn.Close()
+	defer func() {
+		conn.Close()
+		mu.Lock()
+		delete(activeConns, conn)
+		mu.Unlock()
+	}()
 
-	// Buffer to read data
-	buffer := make([]byte, 1024)
+	// 1. Buffer to read data
+	reader := bufio.NewReader(conn) // buffered input output []bytes
 
-	for {
-		// Read data from the client
-		// n = number of bytes read into the buffer- not the size of a tcp packet.
-		n, err := conn.Read(buffer)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-
-		// Process and use the data
-		message := buffer[:n]
-		fmt.Printf("Recieved: %s\n", message)
-
-		// Echo back the received message
-		_, err = conn.Write(message)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
+	// 2. Read the request line (first line) "GET / HTTP/1.1\r\n"
+	requestLine, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading request line:", err)
+		return
 	}
+
+	requestLine = strings.TrimRight(requestLine, "\r\n")
+
+	parts := strings.Split(requestLine, " ")
+	if len(parts) < 3 {
+		fmt.Println("Malformed request line:", requestLine)
+		return
+	}
+
+	method, path, version := parts[0], parts[1], parts[2]
+	fmt.Println("Method: ", method)
+	fmt.Println("Path: ", path)
+	fmt.Println("Version: ", version)
+
+	// Read Headers
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading header:", err)
+			return
+		}
+
+		line = strings.TrimRight(line, "\r\n")
+		if line == "" {
+			break
+		}
+
+	}
+
+	response := "HTTP/1.1 200 OK\r\n"
+	conn.Write([]byte(response))
 }
